28d302b58b43a00e5bbff135a1a37ea89c8956f9
felixsolom <felix.solom@gmail.com>
2025-03-23 14:57:00 +0200

    player ready

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import pygame
 from constants import *
+from player import Player
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+Clock = pygame.time.Clock()
+dt = 0
+
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
+
+x = SCREEN_WIDTH / 2
+y = SCREEN_HEIGHT /2
+player = Player(x, y)
     
 def main():
     while True:
@@ -11,7 +23,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        clock_tick = Clock.tick(60)
+        dt = clock_tick / 1000
+        for item in drawable:
+            item.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
+        
+        
    # print("Starting Asteroids!")
    # print(f"Screen width: {SCREEN_WIDTH}")
    # print(f"Screen height: {SCREEN_HEIGHT}")
